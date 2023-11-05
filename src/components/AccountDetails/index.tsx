7629c4896a4e0da5e752bd91589f04f3d13cc386
Scroll Sale <149031380+Scrollsale@users.noreply.github.com>
2023-11-05 11:50:55 +0800

    Update index.tsx

--- src/components/AccountDetails/index.tsx
+++ src/components/AccountDetails/index.tsx
@@ -53,7 +53,7 @@ const UpperSection = styled.div`
 const InfoCard = styled.div`
   padding: 1rem;
   border: 1px solid ${({ theme }) => theme.bg3};
-  border-radius: 20px;
+  border-radius: 0px;
   position: relative;
   display: grid;
   grid-row-gap: 12px;
@@ -97,8 +97,8 @@ const LowerSection = styled.div`
   flex-grow: 1;
   overflow: auto;
   background-color: ${({ theme }) => theme.bg2};
-  border-bottom-left-radius: 20px;
-  border-bottom-right-radius: 20px;
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
 
   h5 {
     margin: 0;
