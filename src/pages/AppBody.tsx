780ca2fe3c7a27495dae2a0a81487fcce4ca8466
Scroll Sale <149031380+Scrollsale@users.noreply.github.com>
2023-11-05 19:25:20 +0800

    Update AppBody.tsx

--- src/pages/AppBody.tsx
+++ src/pages/AppBody.tsx
@@ -5,7 +5,7 @@ export const BodyWrapper = styled.div`
   position: relative;
   max-width: 30rem;
   width: 100%;
-  border-radius: 1.2rem;
+  border-radius: 0rem;
   box-shadow: rgba(0, 0, 0, 0.01) 0px 0px 1px, rgba(0, 0, 0, 0.04) 0px 4px 8px, rgba(0, 0, 0, 0.04) 0px 16px 24px,
     rgba(0, 0, 0, 0.01) 0px 24px 32px;
   background-color: ${({ theme }) => theme.bg1};
